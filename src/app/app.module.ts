b5110fc58aedc8f871e7294f840005c9ad2f16de
reemfaris <reem.faris97@gmail.com>
2022-07-19 18:16:58 +0200

    ng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
+import { DbService } from './services/db.service';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComComponent } from './first-com/first-com.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    DbService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
